d6b75f3e14e619ccbc2ef4992382e1712a70cf54
Lutfilah Ahmad <lutfilahahmad169@gmail.com>
2026-05-25 22:25:45 +0700

    membuat model

---
+++ task.java
@@ -0,0 +1,18 @@
+ public class Task{
+      private int id;
+      private String title;
+
+      public Task(int id, String title){
+          this.id = id;
+          this.title = title;
+      }
+
+      public int getId(){ return id; }
+      public String getTitle(){ return title; }
+      public void setTitle(String title){ this.title = title; }
+
+      @Override
+      public String toString(){
+          return "Task ID: " + id + " | Title: " + title;
+      }
+  }
